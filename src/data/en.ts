6498c06be60ceb24e05ea9de86dacf19ac33eead
Juliano Braz <jbraz@bionexo.com>
2024-05-22 10:53:44 -0300

    fix: rename and type file

---
+++ src/data/en.ts
@@ -0,0 +1,318 @@
+import MovementDescription from "@/model/MovementDescription";
+
+const DESCRIPTION : MovementDescription []= [
+  {
+    "key": "ox",
+    "name": "ox",
+    "pokemon": "Tauros",
+    "description": "Pour your strength into the form of your Art in its punches, its kicks, in the steady advance of your aggression."
+  },
+  {
+    "key": "crab",
+    "name": "crab",
+    "pokemon": "Kingler",
+    "description": "Move with your opponent's movements, as if you are the never-ceasing tide. When the time is right, he will fall prey to your attack."
+  },
+  {
+    "key": "horse",
+    "name": "horse",
+    "pokemon": "Rapidash",
+    "description": "Lose yourself in the rhythm of your Art. At times be's swift and decisive, at other times measured and taunting."
+  },
+  {
+    "key": "cobra",
+    "name": "cobra",
+    "pokemon": "Arbok",
+    "description": "Attack violently when your opponents are not expecting it... show leisure in the beginning, then suddenly attack vigorously."
+  },
+  {
+    "key": "rabbit",
+    "name": "rabbit",
+    "pokemon": "Lopunny",
+    "description": "Be near to your oponnent, blinding him with your speed. The art of the Rabbit is the Art of speed."
+  },
+  {
+    "key": "dragon",
+    "name": "dragon",
+    "pokemon": "Dragonite",
+    "description": "Be swift as the thunder that peals before you have a change to cover your ears, fast as the lightning that flashses nefore you can blink your eyes."
+  },
+  {
+    "key": "elephant",
+    "name": "elephant",
+    "pokemon": "Donphan",
+    "description": "Only the strong may pursue your Art. This is why it is the true Art, the Art that cannot be stopped."
+  },
+  {
+    "key": "eel",
+    "name": "eel",
+    "pokemon": "Eelektross",
+    "description": "If your opponent strike with fire, counter with water, becoming completely fluid and freeflowing."
+  },
+  {
+    "key": "rooster",
+    "name": "rooster",
+    "pokemon": "Blaziken",
+    "description": "Do not allow your enemy to rest, but focus your Art to deliver quick, sharp strikes whenever he lags."
+  },
+  {
+    "key": "goose",
+    "name": "goose",
+    "pokemon": "Swanna",
+    "description": "Your robes are your cloak of feathers; spread your wings to hide your intentions. Even then, as your opponent seeks to determine your motive, you shall strike."
+  },
+  {
+    "key": "crane",
+    "name": "crane",
+    "pokemon": "Sirfetch’d",
+    "description": "Make no unnecessary movement, converving your strength until the time is right to strike. The true art is a symphony of graceful strikes."
+  },
+  {
+    "key": "boar",
+    "name": "boar",
+    "pokemon": "Emboar",
+    "description": "Watch for opportunity, fot it will present itself. Then strike, focusing all your might into a single rush, trampling your opponent's Art under your own."
+  },
+  {
+    "key": "mantis",
+    "name": "mantis",
+    "pokemon": "Scyther",
+    "description": "Your opponents sees, but does not understand. Distract the watchful, misguide the wary. This is the art of the Mantis, the Art of the deceptive strike."
+  },
+  {
+    "key": "monkey",
+    "name": "monkey",
+    "pokemon": "Infernape",
+    "description": "Without deception you cannot carry out strategy, without strategy you cannot control the opponent."
+  },
+  {
+    "key": "frog",
+    "name": "frog",
+    "pokemon": "Greninja",
+    "description": "Do not fail to learn from the pur voice an ever-flowing mountain stream splashing over the rocks. Emulate is flow, mimc its power."
+  },
+  {
+    "key": "tiger",
+    "name": "tiger",
+    "pokemon": "Raikou",
+    "description": "The power of your Art projects itself like a shadow. Sense your opponent's fear, and puce with certainty and strength."
+  },
+  {
+    "key": "dog",
+    "name": "dog",
+    "pokemon": "Arcanine",
+    "description": "Deny your opponent his Art. Once you have blocked his attack do not let him go, but instead relentlessly hamper his momentum"
+  },
+  {
+    "key": "mouse",
+    "name": "mouse",
+    "pokemon": "Pikachu",
+    "description": "All opponents have a hole in their Art, an opening of weakness. The Art of the Mouse is to find that opening enter it, and strike."
+  },
+  {
+    "key": "phoenix",
+    "name": "phoenix",
+    "pokemon": "Ho-Oh",
+    "description": "No opponent expects that which he believes is not there. Harness the shadows, bend the sund, trike as the wind - from a place unseen."
+  },
+  {
+    "key": "giraffe",
+    "name": "giraffe",
+    "pokemon": "Girafarig",
+    "description": "Project your power, vaulting at opponents who believe themselves safety not of range. Aggression is subservient to surprise"
+  },
+  {
+    "key": "tanuki",
+    "name": "tanuki",
+    "pokemon": "Zigzagoon",
+    "description": "The Art of the Tanuki is the art of opportunity. Patience will open the door to the perfect angle of your strike."
+  },
+  {
+    "key": "iguana",
+    "name": "iguana",
+    "pokemon": "Heliolisk",
+    "description": "While your opponent wears himself out with unnecessary movement, you will gather your strength. A devastating sweep of fatal punch is the pinnacle of the Art."
+  },
+  {
+    "key": "kirin",
+    "name": "kirin",
+    "pokemon": "Xerneas",
+    "description": "There is Art in grace, Art in discernment, Art in sensing the truth. Piece the veil of the shadow, and strike your opponent from a place of enlightenment."
+  },
+  {
+    "key": "otter",
+    "name": "otter",
+    "pokemon": "Buizel",
+    "description": "Who can stop the flowing stream? The Art of the Otter is the Art of Power knocking away the advances of the opponent through sustained, powerful strikes."
+  },
+  {
+    "key": "panda",
+    "name": "panda",
+    "pokemon": "Pangoro",
+    "description": "Your opponent watches for your complancenry, so show it to him. Lull him into an attack, then unleash your Art when he has already commited his attack."
+  },
+  {
+    "key": "fox",
+    "name": "fox",
+    "pokemon": "Ninetales",
+    "description": "Harry your opponent through your Art, ive him no time think, less time to act."
+  },
+  {
+    "key": "rat",
+    "name": "rat",
+    "pokemon": "Raticate",
+    "description": "Your opponent cannot attack what the cannot predict. Let your movements be immediate, your attacks be unexpected."
+  },
+  {
+    "key": "sable",
+    "name": "sable",
+    "pokemon": "Sableye",
+    "description": "Be fluid and control the battlefield with constant movement and precision strikes."
+  },
+  {
+    "key": "sea snake",
+    "name": "sea snake",
+    "pokemon": "Huntail",
+    "description": "There is no movement or opportunity wasted. Strike where you are not expected, flow where your opponent believes you cannot be."
+  },
+  {
+    "key": "turtle",
+    "name": "turtle",
+    "pokemon": "Blastoise",
+    "description": "Within your sphere of power, your Art remains supreme. Engage your opponent, and deflect his strikes."
+  },
+  {
+    "key": "bear",
+    "name": "bear",
+    "pokemon": "Ursaring",
+    "description": "Defend your ground. Do not her hike those fools who are quick to run to attack from another position. They have lost the fight because they have already run from it."
+  },
+  {
+    "key": "viper",
+    "name": "viper",
+    "pokemon": "Seviper",
+    "description": "The Art of the Viper is to strike in the Moment. This is the true Art."
+  },
+  {
+    "key": "bat",
+    "name": "bat",
+    "pokemon": "Crobat",
+    "description": "Do not feat the cowl of the darnkess. Embrance its anonymity and discover its place on the battlefield."
+  },
+  {
+    "key": "eagle",
+    "name": "eagle",
+    "pokemon": "Braviary",
+    "description": "Be majestic and soar above. Attack with talons of expertise."
+  },
+  {
+    "key": "hawk",
+    "name": "hawk",
+    "pokemon": "Talonflame",
+    "description": "Glide through the air, hidden in the mist until it is time to strike. Your prey shall never suspect your swift assault."
+  },
+  {
+    "key": "lion",
+    "name": "lion",
+    "pokemon": "Pyroar",
+    "description": "Your confidence will inspire fear. Your pride will be well earned."
+  },
+  {
+    "key": "octopus",
+    "name": "octopus",
+    "pokemon": "Octillery",
+    "description": "Strecth out and grab every opportunity. Be flexible to what might be found."
+  },
+  {
+    "key": "rhinoceros",
+    "name": "rhinoceros",
+    "pokemon": "Rhyperior",
+    "description": "Charge forward with might and ready a thick skin for all that may challenge your stride."
+  },
+  {
+    "key": "scorpion",
+    "name": "scorpion",
+    "pokemon": "Drapion",
+    "description": "Clamp down your enemies, sting as with a venomous strike. Posson their resolve."
+  },
+  {
+    "key": "spider",
+    "name": "spider",
+    "pokemon": "Ariados",
+    "description": "Set your trap, lie in wait. Time is kind to those with patience and determination."
+  },
+  {
+    "key": "goat",
+    "name": "goat",
+    "pokemon": "Gogoat",
+    "description": "Strategy flows from the mind. Deny your opponents clear thought through strikes at their head and eyes, and their strategy will fail, providing the change to strike."
+  },
+  {
+    "key": "steer",
+    "name": "steer",
+    "pokemon": "Bouffalant",
+    "description": "Your Art comes from your core; only with nourished stomach will you find the strength to conquer ou adversaries."
+  },
+  {
+    "key": "lobster",
+    "name": "lobster",
+    "pokemon": "Clawitzer",
+    "description": "Your Art must be both fluid and precise, like the convergence of Ocean and Air.."
+  },
+  {
+    "key": "cat",
+    "name": "cat",
+    "pokemon": "Meowth",
+    "description": "Avoidance can lead to victory as much as aggression."
+  },
+  {
+    "key": "serow",
+    "name": "serow",
+    "pokemon": "Giratina",
+    "description": "Knowing when to dodge is key to overcoming obstacles."
+  },
+  {
+    "key": "sheep",
+    "name": "sheep",
+    "pokemon": "Ampharos",
+    "description": "Follow your opponent's movements, shadow his forms. The Art of the Sheep is the art of knowing, and of using your opponent's strengths against his weaknesses."
+  },
+  {
+    "key": "mejika",
+    "name": "mejika",
+    "pokemon": "Sawsbuck",
+    "description": "A gentle spirit is often seen as weakness, but it is not so. Show your foes the power of underestimating humility."
+  },
+  {
+    "key": "okija",
+    "name": "okija",
+    "pokemon": "Tangela",
+    "description": "Charge with conviction and strike with the aged weapon of experience."
+  },
+  {
+    "key": "sasori",
+    "name": "sasori",
+    "pokemon": "Gliscor",
+    "description": "Strike swiftly. Strike precisely. Strike with such confidence that your foe will weaken before you have touched them."
+  },
+  {
+    "key": "hornet",
+    "name": "hornet",
+    "pokemon": "Beedrill",
+    "description": "Distract your opponent from above while striking from below. Your venom will weaken their fortitude."
+  },
+  {
+    "key": "centipede",
+    "name": "centipede",
+    "pokemon": "Scolipede",
+    "description": "Mesmerize your opponent with many movements. Your real attack will come unexpectedly."
+  },
+  {
+    "key": "kumo",
+    "name": "kumo",
+    "pokemon": "Galvantula",
+    "description": "Every movement must be part of a plan. The opponent does not know it, but they will see it when it has caught them in defeat."
+  }
+]
+
+export default DESCRIPTION;
