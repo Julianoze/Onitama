bda1590de6a2b1151f887fef1bcfa10df73bb348
Juliano Braz <jbraz@bionexo.com>
2024-05-22 12:34:58 -0300

    improve: add card common props

---
+++ src/app/components/card/CardProps.ts
@@ -0,0 +1,9 @@
+import Description from "@/model/Description";
+import Movement from "@/model/Movement";
+
+interface CardProps {
+  movement : Movement;
+  description : Description;
+}
+
+export default CardProps;
