e864e5be187d524ed14f183a5c21d07651f91605
Juliano Braz <jbraz@bionexo.com>
2024-05-22 13:10:23 -0300

    improve: add portuguese description

---
+++ src/data/pt.ts
@@ -0,0 +1,318 @@
+import MovementDescription from "@/model/MovementDescription";
+
+const DESCRIPTION : MovementDescription[] = [
+  {
+    "key": "ox",
+    "name": "boi",
+    "pokemon": "Tauros",
+    "description": "Derrame sua força na forma da sua Arte nos seus socos,chutes e no avanço constante de sua agressão."
+  },
+  {
+    "key": "crab",
+    "name": "caranguejo",
+    "pokemon": "Kingler",
+    "description": "Mova-se com os movimentos do seu oponente, como se você fosse a maré incessante. Quando o momento for oportuno, ele será vítima de seu ataque."
+  },
+  {
+    "key": "horse",
+    "name": "cavalo",
+    "pokemon": "Rapidash",
+    "description": "Perca-se no ritmo de sua Arte. Às vezes seja rápido e decisivo, em outras vezes medido e provocador."
+  },
+  {
+    "key": "cobra",
+    "name": "cobra",
+    "pokemon": "Arbok",
+    "description": "Ataque violentamente quando seus oponentes não estiverem esperando ... Mostre benevolência no início, então ataque vigorosamente de repente."
+  },
+  {
+    "key": "rabbit",
+    "name": "coelho",
+    "pokemon": "Lopunny",
+    "description": "Esteja perto de seu oponente, cegando-o com sua velocidade. A Arte do Coelho é a Arte da velocidade."
+  },
+  {
+    "key": "dragon",
+    "name": "dragão",
+    "pokemon": "Dragonite",
+    "description": "Seja rápido como o trovão que ruge antes que você tenha chance de cobrir seus ouvidos, rápido como o raio que brilha antes que você possa piscar seus olhos."
+  },
+  {
+    "key": "elephant",
+    "name": "elefante",
+    "pokemon": "Donphan",
+    "description": "Somente os fortes podem seguir sua Arte. É por isso que essa é a verdadeira Arte, a Arte que não pode ser parada."
+  },
+  {
+    "key": "eel",
+    "name": "enguia",
+    "pokemon": "Eelektross",
+    "description": "Se seu oponente atacar com fogo, contra-ataque com água, tornando-se completamente fluido e livre."
+  },
+  {
+    "key": "rooster",
+    "name": "galo",
+    "pokemon": "Blaziken",
+    "description": "Não permita que seu inimigo descanse, mas concentre sua Arte para desferir golpes rápidos e afiados sempre que ele ficar para trás."
+  },
+  {
+    "key": "goose",
+    "name": "ganso",
+    "pokemon": "Swanna",
+    "description": "Suas vestes são seu manto de penas; espalhe suas asas para esconder suas intenções. Mesmo assim, enquanto seu oponente procurar a sua motivação, você irá atacar."
+  },
+  {
+    "key": "crane",
+    "name": "cegonha",
+    "pokemon": "Sirfetch’d",
+    "description": "Não faça nenhum movimento desnecessário, conservando sua força até que seja a hora certa para atacar. A verdadeira Arte é uma sinfonia de golpes graciosos."
+  },
+  {
+    "key": "boar",
+    "name": "javali",
+    "pokemon": "Emboar",
+    "description": "Observe a oportunidade, pois ela se apresentará. Então ataque, concentrando toda a sua força em um único ataque, pisoteando a Arte de seu oponente sob a sua."
+  },
+  {
+    "key": "mantis",
+    "name": "louva-a-deus",
+    "pokemon": "Scyther",
+    "description": "Seu oponente vê, mas não entende. Distraia o vigilante, engane o cauteloso. Esta é a Arte do Louva-a-deus, a Arte do ataque enganoso."
+  },
+  {
+    "key": "monkey",
+    "name": "macaco",
+    "pokemon": "Infernape",
+    "description": "Sem engano, você não pode executar uma estratégia, sem estratégia você não pode controlar o oponente."
+  },
+  {
+    "key": "frog",
+    "name": "sapo",
+    "pokemon": "Greninja",
+    "description": "Não deixe de aprender com a voz pura de um riacho de montanha, que flui constantemente, espirrando sobre as rochas. Emule seu fluxo, imite seu poder."
+  },
+  {
+    "key": "tiger",
+    "name": "tigre",
+    "pokemon": "Raikou",
+    "description": "O poder de sua Arte se projeta como uma sombra. Sinta o medo de seu oponente e avance com certeza e força."
+  },
+  {
+    "key": "dog",
+    "name": "cachorro",
+    "pokemon": "Arcanine",
+    "description": "Negue a seu oponente sua Arte. Depois de bloquear seu ataque, não o deixe ir, mas, em vez disso, impeça incessantemente seu ímpeto."
+  },
+  {
+    "key": "mouse",
+    "name": "rato",
+    "pokemon": "Pikachu",
+    "description": "Todos os oponentes têm um buraco em sua Arte, uma abertura de fraqueza. A Arte do Rato é encontrar essa abertura, entrar nela e atacar."
+  },
+  {
+    "key": "phoenix",
+    "name": "fenix",
+    "pokemon": "Ho-Oh",
+    "description": "Nenhum oponente espera que o que ele acredita que não está lá. Aproveite as sombras, dobre o sol, ataque como o vento de um lugar invisível."
+  },
+  {
+    "key": "giraffe",
+    "name": "girafa",
+    "pokemon": "Girafarig",
+    "description": "Projete seu poder, saltando sobre oponentes que se acreditam estar seguros e fora de alcance. A agressão é subordinada à surpresa."
+  },
+  {
+    "key": "tanuki",
+    "name": "guaxinim",
+    "pokemon": "Zigzagoon",
+    "description": "A Arte do Guaxinim é a arte da oportunidade. A paciência abrirá a porta para o ângulo perfeito de seu ataque."
+  },
+  {
+    "key": "iguana",
+    "name": "iguana",
+    "pokemon": "Heliolisk",
+    "description": "Enquanto seu oponente se desgasta com movimentos desnecessários, você reunirá sua força. Uma soco fatal é o ápice da Arte."
+  },
+  {
+    "key": "kirin",
+    "name": "kirin",
+    "pokemon": "Xerneas",
+    "description": "Há Arte na graça, Arte na discrição, Arte em sentir a verdade. Rasgue o véu da sombra e ataque seu oponente de um lugar de iluminação."
+  },
+  {
+    "key": "otter",
+    "name": "lontra",
+    "pokemon": "Buizel",
+    "description": "Quem pode parar o fluxo do riacho? A Arte da Lontra é a Arte do Poder, afastando os avanços do oponente através de golpes poderosos."
+  },
+  {
+    "key": "panda",
+    "name": "panda",
+    "pokemon": "Pangoro",
+    "description": "Seu oponente procura sua complacência, então mostre-a a ele. Amasse-o em um ataque, então libere sua Arte quando ele já tiver realizado seu ataque."
+  },
+  {
+    "key": "fox",
+    "name": "raposa",
+    "pokemon": "Ninetales",
+    "description": "Assombre seu oponente através de sua Arte, não lhe dê tempo para pensar, muito menos tempo para agir."
+  },
+  {
+    "key": "rat",
+    "name": "rato",
+    "pokemon": "Raticate",
+    "description": "Seu oponente não pode atacar o que não pode prever. Deixe seus movimentos serem imediatos, deixe que seus ataques sejam inesperados."
+  },
+  {
+    "key": "sable",
+    "name": "zibelina",
+    "pokemon": "Sableye",
+    "description": "Seja fluido e controle o campo de batalha com movimento constante e golpes precisos."
+  },
+  {
+    "key": "sea snake",
+    "name": "cobra d' água",
+    "pokemon": "Huntail",
+    "description": "Não há movimento ou oportunidade desperdiçada. Ataque onde você não é esperado, flua onde seu oponente acredita que você não pode estar."
+  },
+  {
+    "key": "turtle",
+    "name": "tartaruga",
+    "pokemon": "Blastoise",
+    "description": "Dentro de sua esfera de poder, sua Arte permanece suprema. Envolva seu oponente e desvie seus golpes."
+  },
+  {
+    "key": "bear",
+    "name": "urso",
+    "pokemon": "Ursaring",
+    "description": "Defenda seu terreno. Não siga aqueles tolos que são rápidos em correr para atacar de outra posição. Eles perderam a luta porque já fugiram dela."
+  },
+  {
+    "key": "viper",
+    "name": "víbora",
+    "pokemon": "Seviper",
+    "description": "A Arte da Víbora é atacar no Momento. Esta é a verdadeira Arte."
+  },
+  {
+    "key": "bat",
+    "name": "morcego",
+    "pokemon": "Crobat",
+    "description": "Não tema o manto da escuridão. Abrace seu anonimato e descubra seu lugar no campo de batalha."
+  },
+  {
+    "key": "eagle",
+    "name": "águia",
+    "pokemon": "Braviary",
+    "description": "Seja majestosa e voe acima. Ataque com garras da sabedoria."
+  },
+  {
+    "key": "hawk",
+    "name": "falção",
+    "pokemon": "Talonflame",
+    "description": "Planeie pelo ar, escondido na névoa até que seja hora de atacar. Sua presa nunca suspeitará de seu ataque rápido."
+  },
+  {
+    "key": "lion",
+    "name": "leão",
+    "pokemon": "Pyroar",
+    "description": "Sua confiança inspirará medo. Seu orgulho será bem merecido."
+  },
+  {
+    "key": "octopus",
+    "name": "polvo",
+    "pokemon": "Octillery",
+    "description": "Estenda-se e aproveite todas as oportunidades. Seja flexível para o que pode ser encontrado."
+  },
+  {
+    "key": "rhinoceros",
+    "name": "rinoceronte",
+    "pokemon": "Rhyperior",
+    "description": "Avance força e prepare uma pele grossa para tudo o que pode desafiar seu passo."
+  },
+  {
+    "key": "scorpion",
+    "name": "escorpião",
+    "pokemon": "Drapion",
+    "description": "Prenda seus inimigos, pique-os. Envenene as resoluções deles."
+  },
+  {
+    "key": "spider",
+    "name": "aranha",
+    "pokemon": "Ariados",
+    "description": "Arme sua armadilha e espere. O tempo é gentil com aqueles que têm paciência e determinação."
+  },
+  {
+    "key": "goat",
+    "name": "bode",
+    "pokemon": "Gogoat",
+    "description": "A estratégia flui da mente. Negue a seus oponentes o pensamento claro através de golpes em sua cabeça e olhos, e sua estratégia falhará, proporcionando a chance de atacar."
+  },
+  {
+    "key": "steer",
+    "name": "boi",
+    "pokemon": "Bouffalant",
+    "description": "Sua Arte vem de seu amâgo; apenas alimentado você encontrará a força para conquistar seus adversários."
+  },
+  {
+    "key": "lobster",
+    "name": "lagosta",
+    "pokemon": "Clawitzer",
+    "description": "Sua Arte deve ser fluida e precisa, como a convergência do Oceano e do Ar."
+  },
+  {
+    "key": "cat",
+    "name": "gato",
+    "pokemon": "Meowth",
+    "description": "A evasão pode levar à vitória tanto quanto a agressão."
+  },
+  {
+    "key": "serow",
+    "name": "serau",
+    "pokemon": "Giratina",
+    "description": "Saber quando desviar é a chave para superar obstáculos."
+  },
+  {
+    "key": "sheep",
+    "name": "ovelha",
+    "pokemon": "Ampharos",
+    "description": "Siga os movimentos de seu oponente, acompanhe suas formas. A Arte da Ovelha é a Arte de conhecer e usar as forças de seu oponente contra suas fraquezas."
+  },
+  {
+    "key": "mejika",
+    "name": "mejika",
+    "pokemon": "Sawsbuck",
+    "description": "Um espírito gentil é muitas vezes visto como fraqueza, mas não é assim. Mostre a seus inimigos o poder de subestimar a humildade."
+  },
+  {
+    "key": "okija",
+    "name": "okija",
+    "pokemon": "Tangela",
+    "description": "Carregue com convicção e ataque com a arma antiga da experiência."
+  },
+  {
+    "key": "sasori",
+    "name": "escorpião",
+    "pokemon": "Gliscor",
+    "description": "Ataque rapidamente. Ataque com precisão. Ataque com tanta confiança que seu inimigo enfraquecerá antes que você os tenha tocado."
+  },
+  {
+    "key": "hornet",
+    "name": "vespa",
+    "pokemon": "Beedrill",
+    "description": "Distraia seu oponente por cima enquanto ataca por baixo. Seu veneno enfraquecerá a coragem dele."
+  },
+  {
+    "key": "centipede",
+    "name": "centopéia",
+    "pokemon": "Scolipede",
+    "description": "Hipnotize seu oponente com muitos movimentos. Seu verdadeiro ataque virá inesperadamente."
+  },
+  {
+    "key": "kumo",
+    "name": "aranha",
+    "pokemon": "Galvantula",
+    "description": "Cada movimento deve ser parte de um plano. O oponente não sabe, mas verá quando for pego na derrota."
+  }
+]
+
+export default DESCRIPTION;
