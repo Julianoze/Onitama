e273c42cbb59348e6145addba008c80d8b5539a2
Juliano Braz <jbraz@bionexo.com>
2024-05-23 16:43:18 -0300

    fix: issue on big screens

--- src/app/components/card/RootCard.tsx
+++ src/app/components/card/RootCard.tsx
@@ -4,7 +4,7 @@ interface RootCardProps {
 
 function RootCard(props : RootCardProps) {
   return (
-    <div className="bg-card border-2 drop-shadow-sm rounded-lg bg-[url('/img/card.png')] bg-no-repeat bg-center p-1 mx-2 my-1 max-w-[500px]">
+    <div className="bg-card border-2 drop-shadow-sm rounded-lg bg-[url('/img/card.png')] bg-no-repeat bg-center p-1 mx-2 my-1 md:w-[500px]">
       {props.children}
     </div>
   )
