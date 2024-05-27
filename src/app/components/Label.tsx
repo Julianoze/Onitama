be626523ce878d7036f51f444db14fa50fc33414
Juliano Braz <jbraz@bionexo.com>
2024-05-27 14:48:37 -0300

    style: format code

--- src/app/components/Label.tsx
+++ src/app/components/Label.tsx
@@ -1,27 +1,35 @@
 interface LabelProps {
-  color : string;
+  class?: string;
+  color: string;
   startWith: string;
-  description: string
+  description: string;
 }
 
-function Label(props : LabelProps) {
-
+function Label(props: LabelProps) {
   const getLabelColor = () => {
-    if(props.color === 'red') return 'bg-red/10';
-    if(props.color === 'blue') return 'bg-blue/10';
-    return 'bg-green/10';
-  }
+    if (props.color === "red") return "bg-red/10";
+    if (props.color === "blue") return "bg-blue/10";
+    return "bg-green/10";
+  };
 
   const getStartWithColor = () => {
-    return props.startWith === 'blue' ? 'bg-blue' : 'bg-red';
-  }
+    return props.startWith === "blue" ? "bg-blue" : "bg-red";
+  };
 
   return (
-    <div className={`flex p-1 h-[100%] justify-around items-center ${ getLabelColor() }`}>
-      <span className="text-[11px] text-center tracking-tighter	">{props.description}</span>
-      <div className={`${getStartWithColor()} min-h-4 max-h-4 min-w-6 m-2`}></div>
+    <div
+      className={`flex p-1 h-[100%] justify-around items-center ${
+        props.class
+      } ${getLabelColor()}`}
+    >
+      <span className="text-[11px] text-center tracking-tighter	">
+        {props.description}
+      </span>
+      <div
+        className={`${getStartWithColor()} min-h-4 max-h-4 min-w-6 m-2`}
+      ></div>
     </div>
-  )
+  );
 }
 
 export default Label;
