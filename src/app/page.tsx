aebe57670ec566c58ea2d9fc8b18717125b457b9
Juliano Braz <jbraz@bionexo.com>
2024-05-27 14:46:33 -0300

    refactor: code formater

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,31 +4,23 @@ import WayOfWindCard from "./components/card/WayOfWindCard";
 import CommonCard from "./components/card/CommonCard";
 
 export default function Home() {
-
   return (
     <div className="flex flex-col w-full ">
       <div className="bg-black"></div>
       <div className="bg-green"></div>
       <div className="bg-cyan-700"></div>
-      {MOVEMENTS
-        .map((x, i) => {
+      {MOVEMENTS.map((x, i) => {
         const description = getDescription(x.name);
 
         return (
           <div className="md:flex w-full justify-center" key={i}>
-            {
-              x.from !== "way-of-wind" ?
-              <CommonCard
-                movement={x}
-                description={description}
-              /> :
-              <WayOfWindCard
-                movement={x}
-                description={description}
-              />
-            }
+            {x.from !== "way-of-wind" ? (
+              <CommonCard movement={x} description={description} />
+            ) : (
+              <WayOfWindCard movement={x} description={description} />
+            )}
           </div>
-        )
+        );
       })}
     </div>
   );
