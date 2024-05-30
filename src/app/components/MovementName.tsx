f22710aefecf8d7adf4874c5bc7b70c28db5d83a
Juliano Braz <jbraz@bionexo.com>
2024-05-30 16:56:14 -0300

    fix: remove class name from movement

--- src/app/components/MovementName.tsx
+++ src/app/components/MovementName.tsx
@@ -1,14 +1,9 @@
 interface MovementNameProps {
-  className?: string;
   name: string;
 }
 
 function MovementName(props: MovementNameProps) {
-  return (
-    <p className={`uppercase font-bold text-black ${props.className}`}>
-      {props.name}
-    </p>
-  );
+  return <p className="uppercase font-bold text-black">{props.name}</p>;
 }
 
 export default MovementName;
