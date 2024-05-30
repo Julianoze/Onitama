b300ca9043dd9333856f04bc5131f5d89f0ae42a
Juliano Braz <jbraz@bionexo.com>
2024-05-30 16:57:24 -0300

    fix: update card min height on mobile

--- src/app/components/card/RootCard.tsx
+++ src/app/components/card/RootCard.tsx
@@ -6,7 +6,7 @@ interface RootCardProps {
 function RootCard(props: RootCardProps) {
   return (
     <div
-      className={`bg-card border-2 drop-shadow-sm rounded-lg bg-[url('/img/card.png')] bg-no-repeat bg-center p-1 mx-2 my-1 md:w-[500px] min-h-[132px] ${props.className}`}
+      className={`bg-card border-2 drop-shadow-sm rounded-lg bg-[url('/img/card.png')] bg-no-repeat bg-center p-1 mx-2 my-1 md:w-[500px] min-h-[110px] ${props.className}`}
     >
       {props.children}
     </div>
