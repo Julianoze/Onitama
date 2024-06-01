75222ec683a99eaf79258d7b9c692f04057c14da
Juliano Braz <jbraz@bionexo.com>
2024-06-01 11:44:02 -0300

    fix: force pokemon version

--- src/handler/Description.ts
+++ src/handler/Description.ts
@@ -1,18 +1,13 @@
 import ENGLISH_DESCRIPTION from "@/data/en";
-import PORTUGUESE_DESCRIPTION from "@/data/pt";
 import Description from "@/model/Description";
 
 function getDescription(key: string): Description {
-  const locale = undefined;
-  const usePokemon = true;
-  const data = !locale ? ENGLISH_DESCRIPTION : PORTUGUESE_DESCRIPTION;
-
-  const movement = data.findLast((x) => x.key === key);
+  const movement = ENGLISH_DESCRIPTION.findLast((x) => x.key === key);
   if (!movement) return {} as Description;
 
   return {
-    useSymbol: !usePokemon,
-    name: usePokemon ? movement.pokemon : movement.name,
+    useSymbol: false,
+    name: movement.pokemon,
     description: movement.description,
   };
 }
